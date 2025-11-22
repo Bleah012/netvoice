a9d1a620cffb5f3c7f27b9602d0f332b951c4741
Bleah012 <blearbarasa@gmail.com>
2025-11-22 13:02:13 +0300

    Add clients.blade.php partial with authentic client/server photos

---
+++ resources/views/partials/clients.blade.php
@@ -0,0 +1,27 @@
+<section class="py-5 bg-light">
+    <div class="container text-center">
+        <h2 class="mb-4">Our Clients</h2>
+        <div class="row g-4">
+            <div class="col-md-3">
+                <img src="{{ asset('images/clients/IMG_20161028_162144.jpg') }}" 
+                     alt="Client A" 
+                     class="img-fluid rounded shadow-sm animate__animated animate__zoomIn">
+            </div>
+            <div class="col-md-3">
+                <img src="{{ asset('images/clients/server1.jpeg') }}" 
+                     alt="Server 1 Infrastructure" 
+                     class="img-fluid rounded shadow-sm animate__animated animate__zoomIn">
+            </div>
+            <div class="col-md-3">
+                <img src="{{ asset('images/clients/server2.jpeg') }}" 
+                     alt="Server 2 Infrastructure" 
+                     class="img-fluid rounded shadow-sm animate__animated animate__zoomIn">
+            </div>
+            <div class="col-md-3">
+                <img src="{{ asset('images/clients/server3.jpeg') }}" 
+                     alt="Server 3 Infrastructure" 
+                     class="img-fluid rounded shadow-sm animate__animated animate__zoomIn">
+            </div>
+        </div>
+    </div>
+</section>
